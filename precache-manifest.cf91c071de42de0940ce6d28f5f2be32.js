067f77ec440f076b1923f929f7c300be77197010
Manoj Prabaharan <manoj.magento@gmail.com>
2020-06-04 23:15:59 +0530

    Updates

---
+++ precache-manifest.cf91c071de42de0940ce6d28f5f2be32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4f33c531d7c58c4b6c795e1f0a1ff1",
+    "url": "/testdrive/index.html"
+  },
+  {
+    "revision": "3abd365c79d0b9b2aca7",
+    "url": "/testdrive/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b3d1eac16b1166efa90",
+    "url": "/testdrive/static/js/2.5942a1a0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testdrive/static/js/2.5942a1a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3abd365c79d0b9b2aca7",
+    "url": "/testdrive/static/js/main.10695417.chunk.js"
+  },
+  {
+    "revision": "d08380097f69feca5606",
+    "url": "/testdrive/static/js/runtime-main.9f36a9ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testdrive/static/media/logo.5d5d9eef.svg"
+  }
+]);
